4366fc917ec0c33f46cf29a039228649f72be59a
Kyle <khartson2017@gmail.com>
2022-07-26 13:29:42 -0400

    commit tree 8c9415a8d7dcf0b2bf5ad43772b3a113a7c7c8b2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # nested routes
+  resources :users, only: [:show] do
+    resources :items, only: [:show, :index]
+  end 
+
+  resources :users, only: [:create] do 
+    resources :items, only: [:create]
+  end 
+
   resources :items, only: [:index]
   resources :users, only: [:show]
 end
